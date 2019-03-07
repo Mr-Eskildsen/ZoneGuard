cdd5f098475fb2bba339af1ae4013138d34f78ef
MartinEskildsen <martin@eskildsen.name>
2019-03-07 18:37:04 +0100

    Add project files.

---
+++ ZoneGuardShared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZoneGuardShared
+{
+    public class Class1
+    {
+    }
+}
---
+++ ZoneGuardShared/Daemon/DaemonConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZoneGuard.Shared.Daemon
+{
+    public class DaemonConfig
+    {
+        public string DaemonName { get; set; }
+    }
+}
