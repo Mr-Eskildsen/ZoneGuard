e30418c8b26759e095a503c5e30e35d13c81c7c6
MartinEskildsen <martin@eskildsen.name>
2019-03-12 22:11:48 +0100

    Web Project

---
+++ ZoneGuard.DAL/Models/Log/SensorStateLogDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using ZoneGuard.DAL.Models.Config;
+
+namespace ZoneGuard.DAL.Models.Log
+{
+    [Table("log_SensorState")]
+    public class SensorStateLogDAL
+    {
+        public int Id { get; set; }
+
+        public ThingDAL Sensor { get; set; }
+
+        [Required]
+        public int Triggered { get; set; }
+        
+        public DateTime CreatedTimestamp { get; set; }
+
+    }
+}
